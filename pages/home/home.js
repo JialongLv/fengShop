54897ea697062db5ff57a6270cc0b12023314ac6
jialong <ljl9410@126.com>
2020-02-04 19:05:07 +0800

    one week

--- pages/home/home.js
+++ pages/home/home.js
@@ -1,66 +1,57 @@
 // pages/home/home.js
+
+import {Theme} from "../../model/theme";
+import {Banner} from "../../model/banner";
+import {Category} from "../../model/category";
+
 Page({
 
-  /**
-   * 页面的初始数据
-   */
-  data: {
+    /**
+     * 页面的初始数据
+     */
+    data: {
+        themeA: null,
+        bannerB: null,
+        grid:[]
+    },
 
-  },
+    /**
+     * 生命周期函数--监听页面加载
+     */
+    async onLoad(options) {
+        this.initAllData()
+    },
 
-  /**
-   * 生命周期函数--监听页面加载
-   */
-  onLoad: function (options) {
 
-  },
+    async initAllData() {
+        const themeA = await Theme.getHomeLocationA();
+        const bannerB = await Banner.getHomeLocationB();
+        const grid = await Category.getHomeLocationC();
+        console.log(grid)
+        this.setData({
+            themeA: themeA[0],
+            bannerB:bannerB,
+            grid:grid
+        })
+    },
+    /**
+     * 页面相关事件处理函数--监听用户下拉动作
+     */
+    onPullDownRefresh: function () {
 
-  /**
-   * 生命周期函数--监听页面初次渲染完成
-   */
-  onReady: function () {
+    },
 
-  },
+    /**
+     * 页面上拉触底事件的处理函数
+     */
+    onReachBottom: function () {
 
-  /**
-   * 生命周期函数--监听页面显示
-   */
-  onShow: function () {
+    },
 
-  },
+    /**
+     * 用户点击右上角分享
+     */
+    onShareAppMessage: function () {
 
-  /**
-   * 生命周期函数--监听页面隐藏
-   */
-  onHide: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面卸载
-   */
-  onUnload: function () {
-
-  },
-
-  /**
-   * 页面相关事件处理函数--监听用户下拉动作
-   */
-  onPullDownRefresh: function () {
-
-  },
-
-  /**
-   * 页面上拉触底事件的处理函数
-   */
-  onReachBottom: function () {
-
-  },
-
-  /**
-   * 用户点击右上角分享
-   */
-  onShareAppMessage: function () {
-
-  }
+    }
 })
---
+++ utils/http.js
@@ -0,0 +1,21 @@
+import {config} from "../config/config";
+import {promisic} from "./util";
+
+class Http {
+    static async request({url, data, callback, method = 'GET'}) {
+        const res = await promisic(wx.request)({
+            url: `${config.apiBaseUrl}${url}`,
+            data,
+            method,
+            header: {
+                appKey: config.appKey
+            }
+        })
+
+        return res.data
+    }
+}
+
+export {
+    Http
+}
